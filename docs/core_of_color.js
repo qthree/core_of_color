a654e5fd34980bcd441160e1027d9b6054827e7f
qthree <qthree3@gmail.com>
2021-10-03 14:55:06 +0700

    Cosmetics

--- docs/core_of_color.js
+++ docs/core_of_color.js
@@ -1239,47 +1239,47 @@ async function init(input) {
         var ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper667 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper666 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 113, __wbg_adapter_28);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper668 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper667 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 113, __wbg_adapter_31);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper670 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper669 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 113, __wbg_adapter_34);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper672 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper671 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 113, __wbg_adapter_37);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper675 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper674 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 113, __wbg_adapter_40);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper677 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper676 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 113, __wbg_adapter_43);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper679 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper678 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 113, __wbg_adapter_46);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper681 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper680 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 113, __wbg_adapter_49);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper683 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper682 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 113, __wbg_adapter_52);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper685 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper684 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 113, __wbg_adapter_55);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper797 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper796 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 178, __wbg_adapter_58);
         return addHeapObject(ret);
     };
--- src/app.rs
+++ src/app.rs
@@ -9,6 +9,7 @@ use hecs::Entity;
 pub struct App {
     state: state::State,
     player: Entity,
+    tick: f64,
 }
 
 impl App {
@@ -16,16 +17,16 @@ impl App {
         let mut state = state::State::default();
         state.batch_spawn_dots(1000);
         let player = state.spawn_player();
-        Self { state, player }
+        Self { state, player, tick: 0.0 }
     }
     fn input_dir(ctx: &egui::CtxRef) -> DVec2 {
         use egui::Key;
         let input = ctx.input();
 
-        let left = input.key_down(Key::ArrowLeft);
-        let right = input.key_down(Key::ArrowRight);
-        let up = input.key_down(Key::ArrowUp);
-        let down = input.key_down(Key::ArrowDown);
+        let left = input.key_down(Key::A) || input.key_down(Key::ArrowLeft);
+        let right = input.key_down(Key::D) || input.key_down(Key::ArrowRight);
+        let up = input.key_down(Key::W) || input.key_down(Key::ArrowUp);
+        let down = input.key_down(Key::S) || input.key_down(Key::ArrowDown);
 
         let left_xor_right = left ^ right;
         let up_xor_down = up ^ down;
@@ -70,6 +71,7 @@ impl epi::App for App {
         self.state.tick();
         let player_pos = self.state.position(self.player).unwrap().vec;
         let player_size = self.state.size(self.player).unwrap().0;
+        let mut misc_dots = player_size.floor();
         let scale = 40.0;
 
         egui::CentralPanel::default()
@@ -90,6 +92,14 @@ impl epi::App for App {
                     (1.0, Color32::from_rgba_premultiplied(10, 10, 10, 10)),
                 );
                 let mut shapes = vec![circle];
+                while misc_dots >= 1.0 {
+                    let frac = std::f64::consts::TAU/3.0;
+                    let transform = |vec| transform(player_pos + vec);
+                    shapes.push(misc_dot(misc_dots, self.tick, transform));
+                    shapes.push(misc_dot(misc_dots, self.tick+frac, transform));
+                    shapes.push(misc_dot(misc_dots, self.tick+frac*2.0, transform));
+                    misc_dots -= 1.0;
+                }
                 let dots = self.state.dots();
                 shapes.extend(dots.iter().map(|dot| {
                     let center = transform(dot.pos.vec);
@@ -98,6 +108,14 @@ impl epi::App for App {
                 }));
                 ui.painter().extend(shapes);
             });
+        self.tick += 0.1;
         ctx.request_repaint();
     }
+}
+
+fn misc_dot(misc_dots: f64, tick: f64, transform: impl Fn(DVec2) -> egui::Pos2) -> egui::Shape {
+    let pos = DVec2::new((- tick + misc_dots * 0.5).cos() * misc_dots * 0.5, (- tick + misc_dots * 0.5).sin() * misc_dots * 0.25);
+    let center = transform(pos*0.5);
+    let color = Color32::from(Color32::WHITE);
+    egui::Shape::circle_filled(center, 1.0, color)
 }
